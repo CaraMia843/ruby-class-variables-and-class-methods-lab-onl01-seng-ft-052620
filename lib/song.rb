b36d25dc040f6c8dc1a035e38b57fd739e3f20c7
Cara Zellner <carajeanine@gmail.com>
2020-06-05 20:50:17 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,19 @@
+class Song
+  attr_accessor :band, :title
+
+  @@all = []
+  
+  def initialize(title, band)
+    self.title = title
+    self.band = band
+  end
+
+  def self.create(title, band)
+    
+
+    Song.new(title, band).tap {|songs| song.save}
+  end
+  
+  def save
+    @@all << self
+  end
